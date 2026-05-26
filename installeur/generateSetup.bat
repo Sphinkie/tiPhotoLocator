8e3bbbfbdf2713887e1ab50f7aa9e51803fa29fe
David de Lorenzo <de.lorenzo.david@gmail.com>
2026-05-26 15:20:12 +0200

    RELEASE v1.7

--- installeur/generateSetup.bat
+++ installeur/generateSetup.bat
@@ -4,7 +4,7 @@
 set "AppName=TiPhotoLocator"
 
 REM del .\packages\sphinkie.%AppName%\data\vc_redist.x64.exe
-C:\Qt\Tools\QtInstallerFramework\4.11\bin\binarycreator --offline-only -c config/config.xml -p packages %AppName%Setup
+C:\Qt\Tools\QtInstallerFramework\4.10\bin\binarycreator --offline-only -c config/config.xml -p packages %AppName%Setup
 
 
 pause
